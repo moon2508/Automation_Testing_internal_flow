0a0ac0ee93b14ef1a9769810787ba726ffeced2e
hangttp25082001 <hanghien25.8@gmail.com>
2023-12-11 14:24:47 +0700

    commit create a order

--- cypress/e2e/createOrder.spec.cy.js
+++ cypress/e2e/createOrder.spec.cy.js
@@ -1,9 +1,13 @@
 describe('Create a order', () => {
   beforeEach(()=>
   {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://192.168.100.192:1999/');
+    cy
   })
   it('create a order', () => {
+    cy.get('#exampleInputEmail1').type('superadmin');
+    cy.get('#exampleInputPassword1').type('123456');
+    cy.get('.btn-success').click();
     
   })
 })
