2c349771595aa4071a35f4058fcde373dc7f73d8
moon2508 <hanghien25.8@gmail.com>
2023-12-12 18:10:25 +0700

    duyệt điều chỉnh đơn hàng - moon2508

--- cypress/e2e/approveAuditOrder.spec.cy.js
+++ cypress/e2e/approveAuditOrder.spec.cy.js
@@ -16,10 +16,10 @@ describe('Approve a audit order', () => {
          .find('a:contains("Danh sách")')
          .click();
     });
-    it('Approve a audit order with status "Chưa điều chỉnh"', () => {
+    it.skip('Approve a audit order with status "Yêu cầu điều chỉnh"', () => {
    
         //filter about status_audit
-        cy.get('#statusAudit_chosen').click().type('Chưa điều chỉnh{enter}');
+        cy.get('#statusAudit_chosen').click().type('Yêu cầu điều chỉnh{enter}');
         cy.get('#searchListOrder').click();
         cy.wait(300);
     
@@ -28,7 +28,37 @@ describe('Approve a audit order', () => {
           cy.log('Tạo đơn điều chỉnh với mã đơn ' + text );
         });;
         cy.get('.text-center').eq(1).click();
-        cy.get('button').contains('Điều chỉnh đơn hàng')
+        cy.get('button').contains('Duyệt điều chỉnh')
+          .click();
+    })
+    it.skip('Reject a audit order with status "Yêu cầu điều chỉnh"', () => {
+   
+        //filter about status_audit
+        cy.get('#statusAudit_chosen').click().type('Yêu cầu điều chỉnh{enter}');
+        cy.get('#searchListOrder').click();
+        cy.wait(300);
+    
+        //detail order
+        cy.get('td.text-center a').eq(1).invoke('text').then((text) => {
+          cy.log('Tạo đơn điều chỉnh với mã đơn ' + text );
+        });;
+        cy.get('.text-center').eq(1).click();
+        cy.get('button').contains('Từ chối điều chỉnh')
+          .click();
+    })
+    it('Cancel a audit order with status "Yêu cầu điều chỉnh"', () => {
+   
+        //filter about status_audit
+        cy.get('#statusAudit_chosen').click().type('Yêu cầu điều chỉnh{enter}');
+        cy.get('#searchListOrder').click();
+        cy.wait(300);
+    
+        //detail order
+        cy.get('td.text-center a').eq(1).invoke('text').then((text) => {
+          cy.log('Tạo đơn điều chỉnh với mã đơn ' + text );
+        });;
+        cy.get('.text-center').eq(1).click();
+        cy.get('button[data-target="#cancelBrowseAudit"]')
           .click();
     })
 
